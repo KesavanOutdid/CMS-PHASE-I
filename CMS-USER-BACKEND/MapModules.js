03a363c03243893e35057c83df5ef7b78ed451d9
vivek-outdid <vivek@outdidtech.com>
2024-02-08 16:46:15 +0530

    charging session - multiple chargers issue fixed

--- CMS-USER-BACKEND/MapModules.js
+++ CMS-USER-BACKEND/MapModules.js
@@ -3,5 +3,7 @@ const wsConnections = new Map();
 const clients = new Map();
 const OCPPResponseMap = new Map();
 const meterValuesMap = new Map();
+const sessionFlags = new Map();
+const charging_states = new Map();
 
-module.exports = { wsConnections, ClientConnections, clients, OCPPResponseMap, meterValuesMap };
+module.exports = { wsConnections, ClientConnections, clients, OCPPResponseMap, meterValuesMap, sessionFlags, charging_states };
